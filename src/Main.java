89079b4ae9b01445131c4e1b60d2a613cc4696dd
YuryShageev <schageev.yur@yandex.ru>
2022-11-09 22:32:13 +0300

    Теоритическая часть по курсовой

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String logo = new String("Как же не хватает часов в сутках!");
+        String logo = ("Как же не хватает часов в сутках!");
 
     }
 }
